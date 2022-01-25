0ee54d0572367c9fd24df2c03dd83dc710da8665
eunlove <mypsj@naver.com>
2022-01-25 17:16:19 +0900

    sj220125 1715: git최초업로

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict"; 
+
+//모듈
+const express = require("express");
+const app = express();
+
+//라우팅
+const home = require("./routes/home"); //소스에서 직접 불러오는거라 경로를 명시해줘야한다
+                                       //현재폴더에서 라우트폴더에서 홈폴더에 있는 자바스크립트를 읽어오도록 
+
+const PORT = 3000;
+
+//앱 세팅
+app.set("views" ,"./views");
+app.set("view engine" , "ejs");
+
+app.use("/" , home); // use : 미들웨어를 등록해주는 메소드 
+
+module.exports = app;
