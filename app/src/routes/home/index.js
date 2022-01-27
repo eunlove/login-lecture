d59146cef19d601d1be688d562e14f71aacb88fe
eunlove <mypsj@naver.com>
2022-01-27 17:19:30 +0900

    sj220127 1719:회원가입

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,6 +6,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login",ctrl.output.login);
+router.get("/register",ctrl.output.register);
 
 router.post("/login",ctrl.process.login); //js/home의 login.js에서 post로 보낸 fetch정보를 받아오기 위함
 
