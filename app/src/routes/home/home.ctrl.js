6b6626e9fddf292130f1f4a02ba9024c8a5dffa9
eunlove <mypsj@naver.com>
2022-01-27 15:49:29 +0900

    sj220127 1549: User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,6 @@
 "use strict";
 
-//유저정보를 가져오기 위해 선언
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req,res) =>{
@@ -18,22 +17,11 @@ const output = {
 
 const process = {
     login:  (req,res) =>{
-        const id = req.body.id,
-          psword = req.body.psword;
-    
-    const users = UserStorage.getUsers("id","psword");
-    
-    const response = {};
-    if(users.id.includes(id)) {
-        const idx = users.id.indexOf(id);
-        if(users.psword[idx]===psword){
-            response.success = true;
-            return res.json(response);
-        }
-    }
-    response.success = false;
-    response.msg = "로그인에 실패하였습니다."
-    return res.json(response);
+
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+        return res.json(response);
     },
 };
     
