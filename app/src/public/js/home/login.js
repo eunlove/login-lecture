e4284a2025ff5abfd0107f3f6b2dd95dafbcaa12
eunlove <mypsj@naver.com>
2022-01-27 10:54:16 +0900

    sj220127 1054:돔을 이용한 HTML객체저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
-"use strict"; 
+"use strict";
 
-console.log("안녕");
-console.log("바이바이");
+const id = document.querySelector("#id"),  //login.ejs에서 id값을 가져올때는 #을 붙여주고 태그를 가져올경우 #을 뺀다.
+psword = document.querySelector("#psword"),
+btn_login = document.querySelector("#btn_login");
+
+btn_login.addEventListener("click" , login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(id.value,psword.value);
+    
+}
